c03e27c249bb5c033d2a123ea2df1a9cbd6f1729
4318471pk <4318471pk@gmail.com>
2023-10-25 20:45:56 +0800

    登录注册

--- Application/ajax_login_register.php
+++ Application/ajax_login_register.php
@@ -1,32 +1,115 @@
 <?php
 include_once("../Public/config.php");
 
-$username=$_POST['username'];
+$loginName=$_POST['username'];
 $password=$_POST['password'];
+$types=$_POST['types'];
+$reapeatpassword=$_POST['reapeatpassword'];
 $agent=$_POST['agent'];
-$roomId=$_POST['roomId'];
+$roomId=$_POST['room'];
 $returnData = array();
 $code=0;
 $msg="success";
 
-if(empty($username) || empty($password) || empty($agent) || empty($roomId))
+
+if(is_null($loginName) || is_null($password) || is_null($agent) || is_null($roomId) || is_null($types))
 {
     $code=-1;
     $msg="参数错误";
+    $returnData=array('code'=>$code,'msg'=>$msg);
+    echo json_encode($returnData);
+    exit();
 }
 
-create(uuid(),$username,$password,"/upload/0.png");
+if($types==0)
+{
+    if(strlen($loginName)<6 || strlen($loginName)>20)
+    {
+        $msg="用户名长度需要在6到20位之间";
+        $code=-6;
+    }
+
+    if(strlen($password)<6 || strlen($password)>20)
+    {
+        $msg="密码长度需要在6到20位之间";
+        $code=-7;
+    }
+
+
+}
+else if($types==1)
+{
+    if(strlen($loginName)<6 || strlen($loginName)>20)
+    {
+        $msg="用户名长度需要在6到20位之间";
+        $code=-2;
+    }
+
+    if($password!=$reapeatpassword)
+    {
+        $msg="两次密码输入不一致！";
+        $code=-3;
+    }
+
+    if(strlen($password)<6 || strlen($password)>20)
+    {
+        $msg="密码长度需要在6到20位之间";
+        $code=-4;
+    }
+
+    if($code==0)
+    {
+        create(uuid(),$loginName,$loginName,$password,"/upload/0.png",$agent);
+    }
+
+}
 
 $returnData=array('code'=>$code,'msg'=>$msg);
+
+if($code==0)
+{
+    $reurl=login($loginName,$password);
+    if(strlen($reurl)>0)
+    {
+        $returnData=array('code'=>$code,'msg'=>$msg,'reurl'=>$reurl);
+    }
+}
+
 echo json_encode($returnData);
 
-function create($userid, $username, $loginpass,$headimg, $agent = "null",$level=1) {
+function create($userid, $username,$loginuser, $loginpass,$headimg, $agent = "null",$level=1) {
     if ($agent == "") {
         $agent = 'null';
     }
-    $username = str_replace('"',"",$username);
+    $loginuser = str_replace(' ',"",$loginuser);
+    $loginpass = str_replace(' ',"",$loginpass);
     //insert_query("fn_user", array("userid" => $userid, 'username' => $username, 'headimg' => $_SESSION['headimg'], 'money' => '0', 'roomid' => $_SESSION['roomid'], 'statustime' => time(), 'agent' => $agent, 'isagent' => 'false', 'jia' => 'false'));
-    insert_query("fn_user", array("userid" => $userid, 'username' => $username,'loginpass' => $loginpass, 'headimg' => $headimg, 'money' => '0', 'roomid' => $roomId, 'statustime' => time(), 'agent' => $agent,'level' => $level, 'isagent' => 'false', 'jia' => 'false'));
+    insert_query("fn_user", array("userid" => $userid, 'loginuser' => $loginuser,'username' => $username,'loginpass' => md5($loginpass), 'headimg' => $headimg, 'money' => '0', 'roomid' => $roomId, 'statustime' => time(), 'agent' => $agent,'level' => $level, 'isagent' => 'false', 'jia' => 'false'));
+    return true;
+}
+
+function login($loginuser,$loginpass) {
+
+    $result=get_query_vals("fn_user","*",array('username' => $loginuser,'loginpass' => md5($loginpass)));
+
+    if (!is_null($result)) {
+        $_SESSION['userid'] = $result['userid'];
+        $_SESSION['username'] = $result['username'];
+        $_SESSION['headimg'] = $result['headimg'];
+        $_SESSION['roomid'] = $result['roomid'];
+        $reurl="../qr.php?room=" . $result['roomid']."&agent=".$result['agent']."&userid=".$result['userid']."&username=".$result['username']."&headimg=".$result['headimg'];
+        return $reurl;
+    } else {
+        return '';
+    }
+}
+
+
+function enter($id) {
+    global $mydb;
+    $_r = $mydb->table('fn_user')->where(array('id' => $id))->find();
+
+
     return true;
 }
 
