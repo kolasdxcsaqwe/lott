0798d2e3689bcffff739f546fa7c86c88f330a24
4318471pk <4318471pk@gmail.com>
2023-11-02 14:52:45 +0800

    sadsad

--- abcaiji/jnd28.php
+++ abcaiji/jnd28.php
@@ -134,29 +134,29 @@ function fadeGenJND28($code)
     -->
 </style>
 <script>
-    var limit = 15
-    if (document.images) {
-        var parselimit = limit
-    }
-
-    function beginrefresh() {
-        if (!document.images)
-            return
-        if (parselimit == 1)
-            window.location.reload()
-        else {
-            parselimit -= 1
-            curmin = Math.floor(parselimit)
-            if (curmin != 0)
-                curtime = curmin + "秒后自动获取!"
-            else
-                curtime = cursec + "秒后自动获取!"
-            timeinfo.innerText = curtime
-            setTimeout("beginrefresh()", 1000)
-        }
-    }
-
-    window.onload = beginrefresh
+    // var limit = 15
+    // if (document.images) {
+    //     var parselimit = limit
+    // }
+    //
+    // function beginrefresh() {
+    //     if (!document.images)
+    //         return
+    //     if (parselimit == 1)
+    //         window.location.reload()
+    //     else {
+    //         parselimit -= 1
+    //         curmin = Math.floor(parselimit)
+    //         if (curmin != 0)
+    //             curtime = curmin + "秒后自动获取!"
+    //         else
+    //             curtime = cursec + "秒后自动获取!"
+    //         timeinfo.innerText = curtime
+    //         setTimeout("beginrefresh()", 1000)
+    //     }
+    // }
+    //
+    // window.onload = beginrefresh
 </script>
 <input type=button name=button value="刷新" onClick="window.location.reload()">
 <span id="timeinfo"></span>
