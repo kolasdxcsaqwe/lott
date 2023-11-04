f52ff21028872a4171fb883a762c5fdb28248781
4318471pk <4318471pk@gmail.com>
2023-11-05 00:00:23 +0800

    sadsad

--- Templates/Old/lottery/ssc.php
+++ Templates/Old/lottery/ssc.php
@@ -370,7 +370,7 @@ function is_weixin()
 
 <!-- New Templates Update -->
 <script type="text/javascript" src="/Style/Old/js/tools.js"></script>
-<script type="text/javascript" src="/Style/Old/js/chat.js?t=23sytf76"></script>
+<script type="text/javascript" src="/Style/Old/js/chat.js?t=sadh3434"></script>
 <script type="text/javascript" src="/Style/Old/js/ssc.js"></script>
 <!-- ./New Templates Update -->
 
