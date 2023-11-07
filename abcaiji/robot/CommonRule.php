0a9841be692db1a1dd01f4fa448420d1f0522497
4318471pk <4318471pk@gmail.com>
2023-11-07 22:53:55 +0800

    sadsad

--- abcaiji/robot/CommonRule.php
+++ abcaiji/robot/CommonRule.php
@@ -4,6 +4,7 @@ include_once dirname(dirname(__FILE__)) . '/Public/config.php';
 
 function startBot($betGame, $roomId,$periodMin,$period)
 {
+    sleep(rand($periodMin,$period));
     select_query('fn_robots','*',"roomid = {$roomId} and game = '{$betGame}' and runstatus = '1' limit 30");
     $arr=array();
     $index=0;
