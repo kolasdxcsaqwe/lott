002ebf13f6cd09c98406e63ddfc364bd722dc5d8
4318471pk <4318471pk@gmail.com>
2023-12-05 18:54:07 +0800

    还原代码

--- clearCache.php
+++ clearCache.php
@@ -40,3 +40,8 @@ $deleteTime=date('Y-m-d H:i:s', time()-259200);//三天前数据删掉
 delete_query('fn_open', "`type`= 1 and `time` < '$deleteTime'");
 echo "清理北京赛车 3天前开奖的数据:".(time()-$time)."秒<br>";
 
+$time=time();
+$deleteTime=date('Y-m-d H:i:s', time()-(60 * 60 * 24 * 15));//15天前数据删掉
+delete_query('fn_userlog', "`addtime` < '$deleteTime'");
+echo "清理用户记录列表 15天前的数据:".(time()-$time)."秒<br>";
+
